392e198629e825c2d710f6b6e0636bc050df8537
Ankur Auti <ankurauti@gmail.com>
2024-09-24 12:21:52 +0530

    changes re done

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,7 @@
-const awsIotConfig = {
-  keyPath: "./certs/test.private.pem.key",
-  certPath: "./certs/test.cert.pem.crt",
-  caPath: "./certs/root-CA.crt",
-  clientId: "mqtt-client-" + Math.floor(Math.random() * 100000 + 1),
-  host: "a3uoz4wfsx2nz3-ats.iot.ap-south-1.amazonaws.com",
-};
+const envConfig = require("./env/development");
 
 module.exports = {
-  awsIotConfig,
+  mongoURI: envConfig.mongoURI,
+  awsIotConfig: envConfig.awsIot,
+  jwtSecret: envConfig.jwtSecret,
 };
